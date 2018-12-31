155691240c9534dced8bd1bfc4242ce5e495bdf5
shiro2212 <nagatomi46@gmail.com>
2018-12-31 11:47:22 +0900

    タップが反応するところまで

---
+++ paplicaMVP/EventListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EventListViewController.swift
+//  paplicaMVP
+//
+//  Created by shinji nagatomi on 2018/12/31.
+//  Copyright © 2018 shinji nagatomi. All rights reserved.
+//
+
+import UIKit
+
+class EventListViewController: UIViewController {
+
+    @IBOutlet weak var selectedUserCard: UIImageView!
+    @IBAction func onTapUserCard(_ sender: UITapGestureRecognizer) {
+        print("タップされました！")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let tapGesture = UITapGestureRecognizer(
+            target: self,
+            action: #selector(self.onTapUserCard(_:)))
+        selectedUserCard.addGestureRecognizer(tapGesture)
+    }
+
+}
+
---
+++ paplicaMVP/SelectUserCardViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SelectUserCardViewController.swift
+//  paplicaMVP
+//
+//  Created by shinji nagatomi on 2018/12/31.
+//  Copyright © 2018 shinji nagatomi. All rights reserved.
+//
+
+import UIKit
+
+class SelectUserCardViewController: UICollectionViewController {
+    
+}
