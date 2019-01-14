4ebc13b1f9ad8a2b6c1208ab7dfe4600f683796c
shiro2212 <nagatomi46@gmail.com>
2019-01-14 11:27:16 +0900

    イベントリストを半動的に変更できるように修正。EventDetailViewも追加

---
+++ paplicaMVP/EventDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EventDetailViewController.swift
+//  paplicaMVP
+//
+//  Created by shinji nagatomi on 2019/01/14.
+//  Copyright © 2019 shinji nagatomi. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var eventDateLabel: UILabel!
+    
+}
--- paplicaMVP/EventListViewController.swift
+++ paplicaMVP/EventListViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class EventListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    // TODO サンプルデータ
+    let eventDate = ["2019/01/14(月)"]
+    let eventImage = ["Umikaze"]
+    let eventTitle = ["うみかぜレンタサイクル"]
+    let eventContent = ["チーム新年会"]
+    
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var selectedUserCard: UIImageView!
     @IBOutlet weak var eventListTable: UITableView!
@@ -29,12 +35,32 @@ class EventListViewController: UIViewController, UITableViewDelegate, UITableVie
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return eventDate.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = eventListTable.dequeueReusableCell(withIdentifier: "event", for: indexPath)
+        // TODO サンプルデータをセットしている処理。差し替える
+        setSampleEventList(cell, cellForRowAt:indexPath)
         return cell
+    }
+    
+    /**
+     * サンプルデータ表示用
+    **/
+    func setSampleEventList(_ cell: UITableViewCell, cellForRowAt indexPath: IndexPath){
+        let dateLabel = cell.contentView.viewWithTag(1) as! UILabel
+        let dateString = eventDate[indexPath.row]
+        dateLabel.text = dateString
+        let imageView = cell.contentView.viewWithTag(2) as! UIImageView
+        let cellImage = UIImage(named: eventImage[indexPath.row])
+        imageView.image = cellImage
+        let titleLabel = cell.contentView.viewWithTag(3) as! UILabel
+        let titleString = eventTitle[indexPath.row]
+        titleLabel.text = titleString
+        let contentLabel = cell.contentView.viewWithTag(4) as! UILabel
+        let contentString = eventContent[indexPath.row]
+        contentLabel.text = contentString
     }
 }
 
